87b45d59aec30796e50ad3a923de8fd54239c90b
kealeee <kean.hin.lee@gmail.com>
2026-04-01 18:42:04 +0800

    fixed (2) dim_oriducts.sql

--- olist_transform/models/marts/dim_products.sql
+++ olist_transform/models/marts/dim_products.sql
@@ -3,6 +3,9 @@
 SELECT
     p.product_id,
     COALESCE(t.product_category_name_english, p.product_category_name) AS product_category_name,
+    p.product_name_lenght,
+    p.product_description_lenght,
+    p.product_photos_qty,
     p.product_weight_g,
     p.product_length_cm,
     p.product_height_cm,
